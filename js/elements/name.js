4a7c0de310b4ee074d2de66aa8c20e416e6eed9d
Ilya Kanatov <ilya.kanatov@gmail.com>
2020-11-24 21:48:56 +0000

    Title is editable now!

--- js/elements/name.js
+++ js/elements/name.js
@@ -3,15 +3,11 @@
 class Name {
 	constructor(_strings) {
 		this._id = _strings.id;
-		this._name = _strings.name;
+		this.name = _strings.name;
 	}
 	get id() {
 		return this._id;
 	}
-	get name() {
-		return this._name;
-	}
-
 	getSerialisedObject() {
 		const object = {
 			id: this.id,
